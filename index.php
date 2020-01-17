c535cdef677da3b835901b9312f1182fa78c0e40
Richelmy <richelmy.monteiro@gmail.com>
2020-01-17 17:01:05 -0300

    Relógio adicionado a página inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<script src='/controle_frios/js/relogio.js'></script>
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<head>
+	<title>Página inicial</title>
+	<?php  
+	require("static/cabecalho_padrao.php");
+
+
+	?>
+
+</head>
+<body onload="relogio()">
+<div id="relogio" class="relogio"></div>
+
+
+</body>
+</html>
