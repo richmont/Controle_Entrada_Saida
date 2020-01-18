55b1c6e0e6c146d4892eb00de8a8f3030ab979c6
Richelmy <richelmy.monteiro@gmail.com>
2020-01-18 14:34:36 -0300

    Listagem dos colaboradores ajustada para módulos
    Quantidade de linhas de código reduzidas e organizadas de maneira eficiente.

--- adm/Colaborador.php
+++ adm/Colaborador.php
@@ -36,12 +36,54 @@ ao lado de cada nome, um botão para apagar cadastro
 
 </div>
 <br>
-		<?php  
-		/**
-		importa e executa a função listar operadores
-		*/
-		require "listar_colaboradores.php";
-		listar_colaboradores_tabela();?> 
+<!-- listagem dos colaboradores deve estar dentro do formulário para apagar -->
+<div class="apagar_colaborador">
+	<form id='form_apagar_colab' action='apagar_colaborador.php'  class='form_apagar_colab' name='form_apagar_colab' method='get'>
+
+			    <table>
+			    <tr>
+				    <th>id</th>
+				    <th>Nome</th>
+				    <th>Matrícula</th>
+				    <th>Apagar</th>
+			    </tr>
+			<?php  
+			/**
+			importa e executa a função listar operadores
+			*/
+			require "listar_colaboradores.php";
+			$lista = listar_colaboradores_array();
+			if($lista != NULL){
+				foreach ($lista as $coluna) {
+					echo "<tr>
+						<td>" . $coluna['id_colaborador'] . "</td>
+						<td>" . $coluna['nome'] . "</td>
+						<td>" . $coluna['matricula'] . "</td>";
+			    	# input invisível cujo valor padrão é o id_colaborador da linha atual
+			    	# permite enviar uma requisição GET com o id do colaborador a ser excluído do banco
+			    	echo "
+			    	<td>
+				    	<input type='hidden' name='id_colaborador' value='" . $coluna['id_colaborador'] . "'>";
+				    	echo "
+				    	<input type='button' value='Apagar' onclick="
+				    	. "\"submitOnClick(confirmarApagarColab('form_apagar_colab'),'form_apagar_colab')\"" .
+				    	"></input>
+
+				    	</tr>";
+				}
+			} else {
+				echo "Tabela vazia, favor cadastrar colaboradores";
+			}
+
+
+
+
+
+
+
+			?> 
+				</table>
+		</form>
 
 </div>
 
--- adm/listar_colaboradores.php
+++ adm/listar_colaboradores.php
@@ -1,60 +1,10 @@
 <script src='/controle_frios/js/validar_form.js'></script>
 <script src="/controle_frios/js/submit_onclick.js"></script>
-<div class="apagar_colaborador">
-<form id='form_apagar_colab' action='apagar_colaborador.php'  class='form_apagar_colab' name='form_apagar_colab' method='get'>
-
-		    <table>
-		    <tr>
-			    <th>id_colaborador</th>
-			    <th>nome</th>
-			    <th>matrícula</th>
-			    <th>apagar</th>
-			    <th>alterar</th>
-		    </tr>
-
 
 <?php  
 require "../db/db_conexao.php";
 $conexao = conectar_banco($db_credenciais);
 mysqli_select_db ( $conexao , $db_credenciais["database"] );
-
-function listar_colaboradores_tabela(){
-	global $conexao;
-
-	$query_listar_colab = "SELECT id_colaborador, nome, matricula FROM colaboradores ORDER BY id_colaborador DESC;";
-
-	$r_listar_colab = mysqli_query($conexao, $query_listar_colab);
-
-	if(!$r_listar_colab){
-		print("Erro: " . mysqli_error($conexao));
-	} else {
-		if (mysqli_num_rows($r_listar_colab) > 0) {
-		    // saída dos dados de cada coluna
-		    while($coluna = mysqli_fetch_assoc($r_listar_colab)) {
-		    	echo "<tr>";
-		    	echo "<td>" . $coluna['id_colaborador'] . "</td>";
-		    	echo "<td>" . $coluna['nome'] . "</td>";
-		    	echo "<td>" . $coluna['matricula'] . "</td>";
-		    	# input invisível cujo valor padrão é o id_colaborador da linha atual
-		    	# permite enviar uma requisição GET com o id do colaborador a ser excluído do banco
-		    	echo "
-		    	<td>
-			    	<input type='hidden' name='id_colaborador' value='" . $coluna['id_colaborador'] . "'>";
-
-			    	echo "
-			    	<input type='button' value='Apagar' onclick="
-			    	. "\"submitOnClick(confirmarApagarColab('form_apagar_colab'),'form_apagar_colab')\"" .
-			    	"></input>
-
-			    	</tr>";
-		    }
-		    echo "</table></form></div>";
-		} else {
-		    echo "Nenhuma entrada";
-			}
-		
-	}
-}
 
 function listar_colaboradores_array(){
 	/**
@@ -62,27 +12,22 @@ function listar_colaboradores_array(){
 	ordena pelo id_colaborador, retorna ao usuário um array.
 	*/
 	global $conexao;
-	$lista_colaboradores = array();
-	$query_listar_colab = "SELECT id_colaborador, nome, matricula FROM colaboradores ORDER BY id_colaborador DESC;";
-
+	$lista_colaboradores = [];
+	$query_listar_colab = "SELECT * FROM colaboradores ORDER BY id_colaborador DESC;";
 	$r_listar_colab = mysqli_query($conexao, $query_listar_colab);
-
-	$array_tabela = array();
 	if(!$r_listar_colab){
 		print("Erro: " . mysqli_error($conexao));
 	} else {
 		if (mysqli_num_rows($r_listar_colab) > 0) {
 		    // saída dos dados de cada coluna
 		    while($coluna = mysqli_fetch_assoc($r_listar_colab)){
-		    	# recebe linha por linha da consulta e insere no fim de um array
-		    	 array_push($tabela, $coluna);
+		    	array_push($lista_colaboradores, $coluna);
 		    }
-		    return $array_tabela;
+		    return $lista_colaboradores;
 		    
 		}
 	}
 }
-
 
 function colaborador_nome($id_colaborador){
 	# consulta nome do colaborador informando o id
