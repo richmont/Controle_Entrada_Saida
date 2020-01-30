a83b0c101c68b7687f332a5de59ea6119d6df5a4
Richelmy <richelmy.monteiro@gmail.com>
2020-01-30 17:41:03 -0300

    Script melhorado para incluir horário, data e guardar log de erro

--- backup_database.ps1
+++ backup_database.ps1
@@ -1,16 +1,32 @@
 <# 
 
 #>
+$local_atual = Get-Location
+
 $mysql_bin = "C:\xampp\mysql\bin"
-$data = Get-Date -UFormat "%d-%m-%Y"
+$data = Get-Date -UFormat "%d-%m-%Y_%Hh%Mm"
 $localizacao = "localhost"
 $usuario = "root"
-$senha = "<sua senha>"
+$senha = "<senha>"
 $database = "controle_frios"
-$local_backup = "C:\Users\<seu usuario>"
-
-$comando = ".\mysqldump -h $localizacao -u $usuario --databases $database > $local_backup\$database-$data.sql -p$senha"
-
+$local_backup = "C:\"
+$log_file = "$database-$data.log" 
+$output_file = "$database-$data.sql"
+$comando = ".\mysqldump -h $localizacao -u $usuario --databases $database  -p$senha > $output_file 2> $local_backup$log_file "
 
 Set-Location -Path $mysql_bin
 Invoke-Expression $comando
+
+if ( (get-childitem $log_file).length -eq 0 )
+  {
+    Compress-Archive -Path $output_file -CompressionLevel Optimal -DestinationPath $local_backup$output_file".zip"
+    Remove-Item $output_file
+    "Backup realizado com sucesso"
+
+    
+  }else{
+    "Erro verifique o log"
+
+  }
+
+Set-Location $local_atual
