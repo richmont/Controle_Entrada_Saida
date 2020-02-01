bae7d020a7bc7773a00e7442cb14352726d8e007
Richelmy <richelmy.monteiro@gmail.com>
2020-02-01 11:19:04 -0300

    Comentários ao que cada comando do script faz

--- backup_database.ps1
+++ backup_database.ps1
@@ -1,32 +1,45 @@
 <# 
-
+Script powershell simples para execução do MYSQLDUMP e compactação em zip
 #>
-$local_atual = Get-Location
 
+# guarda o local atual do terminal Powershell antes de executar o comando
+$local_atual = Get-Location
+# local dos executáveis MySQL
 $mysql_bin = "C:\xampp\mysql\bin"
+
+# data de hoje em formado dia-mes-ano_hora_minuto
 $data = Get-Date -UFormat "%d-%m-%Y_%Hh%Mm"
+# endereço do servidor mysql
 $localizacao = "localhost"
+# credenciais
 $usuario = "root"
 $senha = "<senha>"
+# base de dados a receber backup
 $database = "controle_frios"
-$local_backup = "C:\"
+# local onde vai ser armazenado o backup e o log
+$local_backup = "C:\Users\richel277287\Documents\"
+# nome do arquivo de log e do backup
 $log_file = "$database-$data.log" 
 $output_file = "$database-$data.sql"
+# comando completo do mysqldump
 $comando = ".\mysqldump -h $localizacao -u $usuario --databases $database  -p$senha > $output_file 2> $local_backup$log_file "
-
+# define localização para pasta de executáveis do mysql
 Set-Location -Path $mysql_bin
+# executa o comando
 Invoke-Expression $comando
-
+# checa se o arquivo de log é maior que 0 bytes
 if ( (get-childitem $log_file).length -eq 0 )
   {
+    # se o arquivo de log não reportar erros, 
+    # compacta o backup em formato SQL para zip, e copia pra pasta de backup
     Compress-Archive -Path $output_file -CompressionLevel Optimal -DestinationPath $local_backup$output_file".zip"
+    # remove o arquivo de backup SQL da pasta de executáveis do MySQL
     Remove-Item $output_file
     "Backup realizado com sucesso"
-
-    
   }else{
+    # imprime na tela um aviso para verificar o log, pois houve um erro no backup
     "Erro verifique o log"
 
   }
-
+# retorna o caminho do terminal Powershell para onde estava antes de rodar o comando
 Set-Location $local_atual
