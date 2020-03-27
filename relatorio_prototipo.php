2814019622693194e87b727bea8520a2a00f7ed3
Richelmy <richelmy.monteiro@gmail.com>
2020-03-27 12:17:52 -0300

    Protótipo da página de relatório resultante do uso do sistema.

---
+++ relatorio_prototipo.php
@@ -0,0 +1,31 @@
+<html>
+    <head><title>Relatório de Richelmy Monteiro</title></head>
+        <body>
+
+        <table width=100% align=center>
+            <tr>
+                <td><img src="atacadao.png" alt="Logo atacadão" width=20% align=left></td>
+                
+            </tr>
+            </table>
+            <table border=1 width=100% align=center>
+            
+            
+                <tr>
+                    <th rowspan=2 align=center>Nome</th><th colspan=2 align=center>Horário</th>
+                    </tr>
+                <tr>
+                    <th align=center>Ida</th><th align=center>Volta</th>
+                    </tr>
+                <tr>
+                    <td>Richelmy</td><td>11:00</td><td>11:05</td>
+                    </tr>
+                <tr>
+                    <td>Richelmy</td><td>11:30</td><td>11:35</td>
+                    </tr>
+                <tr>
+                    <td>Richelmy</td><td>12:30</td><td>12:35</td>
+                    </tr>    
+            </table>
+        </body>
+</html>
