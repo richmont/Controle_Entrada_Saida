e0dbfa63e623b3256174ef8a8ca585661b6e2ff4
Richelmy <richelmy.monteiro@gmail.com>
2020-03-28 12:20:35 -0300

    Template da página de relatório pronto

---
+++ adm/gerar_relatorio.php
@@ -0,0 +1,33 @@
+<html>
+    <head><title>Relatório de Colaborador</title></head>
+        <body>
+        <link rel="stylesheet" type="text/css" href="../css/relatorios.css">
+        <table class="tab_cabecalho">
+            <tr>
+                <!-- <td><img class="logo_atacadao" src="../atacadao.png" alt="Logo atacadão"></td> -->
+                
+            </tr>
+            </table>
+            <div class="relat-body">
+            <table>
+                <tr><th>Data</th><th colspan=2>28/03/20</th></tr>
+                <tr><td>Assinatura</td><td colspan=2></td></tr>
+                    <tr>
+                        <th rowspan=2 >Nome</th><th colspan=2 >Horário</th>
+                        </tr>
+                    <tr>
+                        <th >Ida</th><th >Volta</th>
+                        </tr>
+                    <tr>
+                        <td>Richelmy</td><td>11:00</td><td>11:05</td>
+                        </tr>
+                    <tr>
+                        <td>Richelmy</td><td>11:30</td><td>11:35</td>
+                        </tr>
+                    <tr>
+                        <td>Richelmy</td><td>12:30</td><td>12:35</td>
+                        </tr>    
+                </table>
+            </div>
+        </body>
+</html>
