b2ed93c81371f0031b60217a65f51f2534d97064
Richelmy <richelmy.monteiro@gmail.com>
2020-03-28 14:19:11 -0300

    Gerando planilha

--- adm/gerar_relatorio.php
+++ adm/gerar_relatorio.php
@@ -4,30 +4,67 @@
         <link rel="stylesheet" type="text/css" href="../css/relatorios.css">
 
             <div class="relat-body">
-            <table>
-                <tr><th>Data</th><th colspan=2>28/03/20</th></tr>
-                <tr class="linha_assinatura"><td>Assinatura</td><td colspan=2></td></tr>
-                    <tr>
-                        <th rowspan=2 >Nome</th><th colspan=2 >Horário</th>
-                        </tr>
-                    <tr>
-                        <th >Ida</th><th >Volta</th>
-                        </tr>
+            
 
                         <?php
                         set_include_path($_SERVER['DOCUMENT_ROOT'] . "/Controle_Entrada_Saida/") ;
                         require_once("registro/listar_registros.php");
-                        
+                        require_once("listar_colaboradores.php");
+                        $agora = new datetime();
+                        $timezone = new datetimezone('America/Belem');
+                        $agora->settimezone($timezone);
+                        #echo $agora->format('d-m-Y H:i:s');
+                        $agora->format('d-m-Y');
+                        $dia = $agora->format('d');
+                        $mes = $agora->format('m');
+                        $ano = $agora->format('y');
+                        $bool = empty($_GET["matricula"]);
+                         // verifica se há valores recebidos por GET
+                        if($bool){
+                            #echo "Matrícula ausente, insira na tela de registro";
+                        } else{
+                            $matricula = $_GET["matricula"];
+                            # verificação via regex se o valor recebido é válido
+                            # matrícula deve conter apenas números
+                            $array_saida_matricula = array();
+                            $regex_matricula = '/[a-zA-Z!@#$%^&*(),.?":{}|<>]/';
+
+                            preg_match_all($regex_matricula, $matricula, $array_saida_matricula);
+                            # se valor for maior que zero, a matrícula não contém apenas números
+                            if(sizeof($array_saida_matricula[0]) > 0){
+                                echo 'Matrícula deve conter apenas números';
+                                
+                            } else{
+                                # verifica no banco se matrícula corresponde a um colaborador
+                                $id_colaborador = colaborador_id_pela_matricula($matricula);
+                                if($id_colaborador == NULL){
+                                    echo "Matrícula inexistente";
+                                } else{
+                                    # matrícula encontrada, finalmente
+                                    
+                                    #echo "Colaborador de código ". $id_colaborador. " recebido";
+                                    echo "
+                                    <table>
+                                    <tr><th>Data</th><th colspan=2>" .$dia."/".$mes."/".$ano."</th></tr>
+                                    <tr class='linha_assinatura'><td>Assinatura</td><td colspan=2></td></tr>
+                                    <tr>
+                                    <th rowspan=2 >Nome</th><th colspan=2 >Horário</th>
+                                    </tr>
+                                    <tr>
+                                    <th >Ida</th><th >Volta</th>
+                                    </tr>
+                                    ";
+                                    $lista = listar_registro_data_colaborador($dia,$mes,$ano,$id_colaborador);
+                                    foreach($lista as $coluna){
+                                        echo "<tr><td>".$coluna["nome"]."</td><td>".$coluna["hora_entrada"]."</td><td>".$coluna["hora_saida"]."</td>";
+                                    }
+
+		                        }
+	                        }			
+                        }   
+
                         ?>
-                    <tr>
-                        <td>Richelmy</td><td>11:00</td><td>11:05</td>
-                        </tr>
-                    <tr>
-                        <td>Richelmy</td><td>11:30</td><td>11:35</td>
-                        </tr>
-                    <tr>
-                        <td>Richelmy</td><td>12:30</td><td>12:35</td>
-                        </tr>    
+                    
                 </table>
             </div>
         </body>
