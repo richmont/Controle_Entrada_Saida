397de40737e78baef4894c5f39b361acddf30283
Richelmy <richelmy.monteiro@gmail.com>
2020-03-31 07:21:37 -0300

    Rodapé atualizado para incluir contribuições de todo o setor de CPD

--- static/rodape.php
+++ static/rodape.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="/Controle_Entrada_Saida/css/navbar.css">
 <div class="rodape">
 	<ul>
-    <li><a href="https://github.com/richmont/Controle_Entrada_Saida">Todos os direitos reservados a Richelmy Monteiro (Filial 238)</a></li>
+    <li><a href="https://github.com/richmont/Controle_Entrada_Saida">Desenvolvido CPD Ananindeua Castanheira - 238</a></li>
     <li></li>
 	  
 	</ul>
