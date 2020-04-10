a93f22eecf19c9f2a1d28619451f982bbb060ef1
Richelmy <richelmy.monteiro@gmail.com>
2020-04-10 10:19:12 -0300

    Botão para gerar relatório geral do dia

--- adm/Colaborador.php
+++ adm/Colaborador.php
@@ -25,16 +25,17 @@ ao lado de cada nome, um botão para apagar cadastro
 	Botão que roda a função em js para alterar o atributo display, de hidden para block
 	-->
 	<input type="button" class="btnMostrarFormCadastro" name="btnMostrarCadastro" value="Cadastrar Colaborador" onclick="esconderElemento('form_in_colab')"></input>
-	
+	<input type="button" class="btnRelatorioGeral" name="btnRelatorioGeral" value="Gerar relatório geral" onclick="location.href = 'gerar_relatorio.php';"></input>
 	<div id="form_in_colab" class="form_in_colab" style="display: none;">
 		<form action="cadastro_colaborador.php" method="get" id="form_colaborador">
 			<ul>
 				<li>Nome: <input type="text" name="nome" id="inputNome"></li>
 				<li>Matrícula: <input type="text" name="matricula" id="inputMatricula"></li>
 				<li><input type="submit" value="Cadastrar" onfocus="validar_cadastro()" onmouseover="validar_cadastro()"></input></li>
+				
 			</ul>
 		</form>
-
+		
 </div>
 <br>
 <!-- listagem dos colaboradores deve estar dentro do formulário para apagar -->
